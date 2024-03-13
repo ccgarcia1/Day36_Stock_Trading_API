1aa452680b9abba9a9f1518f19f48fa340275c09
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-03-12 22:47:07 -0300

    Getting the news and sending SMS message with the 3 first news

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import requests
 from datetime import datetime, timedelta
+from twilio.rest import Client
+
+from keys import Keys
+new_key = Keys()
+
 STOCK_NAME = "DELL"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+stock_api_key = new_key.stock_api_key
+news_api_key = new_key.news_api_key
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-# Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_price for (key, value) in dictionary.items()]
-stock_api_key = ""
+# Get yesterday's closing stock price. Hint: You can perform list
+# comprehensions on Python dictionaries. e.g. [new_price for (key, value) in dictionary.items()]
+
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -22,54 +29,65 @@ stock_params = {
 stock_response = requests.get(STOCK_ENDPOINT, stock_params)
 stock_response.raise_for_status()
 stock_data = stock_response.json()['Time Series (Daily)']
-yesterday_date = (datetime.today().date() - timedelta(days=3)).strftime("%Y-%m-%d")
+yesterday_date = (datetime.today().date() - timedelta(days=5)).strftime("%Y-%m-%d")
 yesterday_stock = stock_data.get(yesterday_date, None)
 yesterday_closing_price = float(yesterday_stock["4. close"])
 
 
-#TODO 2. - Get the day before yesterday's closing stock price
+# Get the day before yesterday's closing stock price
 day_before_yesterday_date = (datetime.today().date() - timedelta(days=4)).strftime("%Y-%m-%d")
 day_before_yesterday_stock = stock_data.get(day_before_yesterday_date, None)
 day_before_yesterday_closing_price = float(day_before_yesterday_stock["4. close"])
-print(day_before_yesterday_closing_price)
 
-# - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+print(f"Yesterday closing price: {yesterday_closing_price}")
+print(f"Day before yesterday closing price: {day_before_yesterday_closing_price}")
+
+# - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20,
+# but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = (yesterday_closing_price - day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "☝ 🚀"
+else:
+    up_down = "👎 😨"
 
-print(difference)
+print(f"Difference between both prices: {difference}")
 
-#- Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_perc = (difference / yesterday_closing_price) * 100
+# Work out the percentage difference in price between closing price yesterday
+# and closing price the day before yesterday.
+diff_perc = round((difference / yesterday_closing_price) * 100, 2)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_perc > 5:
-    print("Get News")
-    ## STEP 2: https://newsapi.org/
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+print(f"Percentage of difference between: {diff_perc}")
+# If TODO4 percentage is greater than 5 then print("Get News").
+if abs(diff_perc) > 3:
+    news_params = {
+        "qInTitle": STOCK_NAME,
+        "apiKey": news_api_key
+    }
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    news_response = requests.get(NEWS_ENDPOINT, news_params)
+    news_response.raise_for_status()
+    news_data = news_response.json()["articles"]
+    # print(news_data)
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    # Use Python slice operator to create a list that contains the first 3 articles.
+    # Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = news_data[:3]
+    # print(three_articles)
+    # STEP 3: Use twilio.com/docs/sms/quickstart/python
+    # To send a separate message with each article's title and description to your phone number.
 
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles_list = [f"\n{STOCK_NAME}: {up_down}{diff_perc}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles_list)
+    # Send each article as a separate message via Twilio.
+    # sending SMS via Twilio
+    client = Client(new_key.twilio_account_sid, new_key.twilio_auth_token)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
+    for article in formatted_articles_list:
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio.
-
-
-
-#Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
-
+        message = client.messages.create(
+            body=article,
+            from_=new_key.twilio_from,
+            to=new_key.twilio_to
+        )
